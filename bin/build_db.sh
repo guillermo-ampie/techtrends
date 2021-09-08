3363078c7a8e6bd38e69d40e1f92e870e01b82fe
Guillermo Ampie <ga.ampie@gmail.com>
2021-09-07 19:21:31 -0500

    Move build_db.sh to "bin" directory: update file references accordingly

---
+++ bin/build_db.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+DIR=techtrends
+
+{
+    test -e ${DIR}/database.db &&
+        echo ">>> Database already created!"
+} ||
+    {
+        echo -n ">>> Building database..."
+        cd ${DIR} && python init_db.py
+        echo "done!"
+    }
